550ed7439652032dca4dbee8d9bf6cd8924309d1
Sriram gonuguntla <chinnu.sriram85@gmail.com>
2025-08-10 02:23:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ function formValidation() {
     var username = document.forms["myForm"]["username"].value;
     var email = document.forms["myForm"]["email"].value;
     var password = document.forms["myForm"]["password"].value;
-    var confirmpassword = document.forms["myForm"]["confirmpassword"].value
-    var phonenumber = document.forms["myForm"]["phonenumber"].value;
 
     //for giving alert -step 2
     //syntax: /^ $/
@@ -12,7 +10,6 @@ function formValidation() {
     var usernameRegex = /^[a-zA-Z0-9]{3,}$/; //for Username
     var emailRegex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/;        //email
     var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$#_]).{6,8}$/;     //password
-    var phonenumberRegex = /^[0-9]{10}$/;
 
 
 
@@ -30,14 +27,14 @@ function formValidation() {
         alert("password is wrong");
         return false;
     }
-    if (confirmpassword !== password) {
-        alert("password doesn't match");
-        return false;
-    }
-    if (!phonenumberRegex.test(phonenumber)) {
-        alert("enter correct phonenumber");
-        return false;
-    }
-    alert("Form submitted successfully" + username + email + password + confirmpassword + phonenumber);
+    alert("Login Successful" + username  + email  + password);
     return true;
 }
+
+
+
+
+
+
+
+
